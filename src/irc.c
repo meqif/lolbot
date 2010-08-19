987c83466f76325c2fc5ad7aaf6f876e7cf1e666
Ricardo Martins <ricardo@scarybox.net>
2010-08-20 00:21:00 +0100

    irc.c uses bstring.

--- src/irc.c
+++ src/irc.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <bstring.h>
 
 #define MAX_FILE_SIZE 20 /* (2**64).to_s.length */
 #ifndef DEBUG
-    #define SEND(socket, message) send(socket, message, strlen(message), 0)
+    #define SEND(socket, message) send(socket, message->data, blength(message), 0)
 #else
-    #define SEND(socket, message) fprintf(stderr, "%s", message); send(socket, message, strlen(message), 0)
+    #define SEND(socket, message) fprintf(stderr, "%s", message->data); send(socket, message->data, blength(message), 0)
 #endif
 
 int irc_user(int sockfd, char *nick, char *real_name)
 {
     int err;
-    char *message;
+    bstring message;
 
-    message = calloc(11 + strlen(nick) + strlen(real_name) + 2 + 1, sizeof(char));
-    sprintf(message, "USER %s x x :%s\r\n", nick, real_name);
+    message = bformat("USER %s x x :%s\r\n", nick, real_name);
     err = SEND(sockfd, message);
-    free(message);
+    bdestroy(message);
 
     return err;
 }
@@ -26,12 +26,11 @@ int irc_user(int sockfd, char *nick, char *real_name)
 int irc_nick(int sockfd, char *nick)
 {
     int err;
-    char *message;
+    bstring message;
 
-    message = calloc(7 + strlen(nick) + 1, sizeof(char));
-    sprintf(message, "NICK %s\r\n", nick);
+    message = bformat("NICK %s\r\n", nick);
     err = SEND(sockfd, message);
-    free(message);
+    bdestroy(message);
 
     return err;
 }
@@ -39,30 +38,34 @@ int irc_nick(int sockfd, char *nick)
 int irc_join(int sockfd, char *channel)
 {
     int err;
-    char *message;
+    bstring message;
 
-    message = calloc(8 + strlen(channel) + 1, sizeof(char));
-    sprintf(message, "JOIN #%s\r\n", channel);
+    message = bformat("JOIN #%s\r\n", channel);
     err = SEND(sockfd, message);
-    free(message);
+    bdestroy(message);
 
     return err;
 }
 
 int irc_quit(int sockfd)
 {
-    return SEND(sockfd, "QUIT\r\n");
+    int err;
+    bstring message;
+
+    message = bfromcstr("QUIT\r\n");
+    err = SEND(sockfd, message);
+
+    return err;
 }
 
 int irc_pong(int sockfd, char *server)
 {
     int err;
-    char *message;
+    bstring message;
 
-    message = calloc(strlen("PONG ") + strlen(server) + 3, sizeof(char));
-    sprintf(message, "PONG %s\r\n", server);
+    message = bformat("PONG %s\r\n", server);
     err = SEND(sockfd, message);
-    free(message);
+    bdestroy(message);
 
     return err;
 }
@@ -70,13 +73,11 @@ int irc_pong(int sockfd, char *server)
 int irc_privmsg(int sockfd, char *remote_nick, char *content)
 {
     int err;
-    char *message;
+    bstring message;
 
-    message = calloc(12 + strlen(content) + strlen(remote_nick) + 1,
-            sizeof(char));
-    sprintf(message, "PRIVMSG %s :%s\r\n", remote_nick, content);
+    message = bformat("PRIVMSG %s :%s\r\n", remote_nick, content);
     err = SEND(sockfd, message);
-    free(message);
+    bdestroy(message);
 
     return err;
 }
@@ -84,13 +85,11 @@ int irc_privmsg(int sockfd, char *remote_nick, char *content)
 int irc_notice(int sockfd, char *remote_nick, char *content)
 {
     int err;
-    char *message;
+    bstring message;
 
-    message = calloc(11 + strlen(content) + strlen(remote_nick) + 1,
-            sizeof(char));
-    sprintf(message, "NOTICE %s :%s\r\n", remote_nick, content);
+    message = bformat("NOTICE %s :%s\r\n", remote_nick, content);
     err = SEND(sockfd, message);
-    free(message);
+    bdestroy(message);
 
     return err;
 }
@@ -99,14 +98,12 @@ int irc_dcc_send(int sockfd, char *remote_nick, char *filename,
                  unsigned long filesize, unsigned int address, int port)
 {
     int err;
-    char *message, *command = "DCC SEND";
+    char *command = "DCC SEND";
+    bstring message;
 
-    size_t size = strlen(command) + strlen(filename) + 10 +
-                  strlen("65535") + MAX_FILE_SIZE + 5 + 2;
-    message = calloc(size+1, sizeof (char));
-    snprintf(message, size, "%c%s %s %u %d %lu%c", '\1', command,
-            filename, address, port, filesize, '\1');
-    err = irc_privmsg(sockfd, remote_nick, message);
+    message = bformat("\01%c%s %s %u %d %lu%c\01", command,
+            filename, address, port, filesize);
+    err = irc_privmsg(sockfd, remote_nick, bdata(message));
 
     return err;
 }
@@ -115,14 +112,12 @@ int irc_dcc_accept(int sockfd, char *remote_nick, char *filename,
                  int port, unsigned long resume_offset)
 {
     int err;
-    char *message, *command = "DCC ACCEPT";
+    char *command = "DCC ACCEPT";
+    bstring message;
 
-    size_t size = strlen(command) + strlen(filename) + 20 +
-                  strlen("65535") + 5 + 2;
-    message = calloc(size+1, sizeof (char));
-    snprintf(message, size, "\01%s %s %d %lu\01", command,
+    message = bformat("\01%s %s %d %lu\01", command,
             filename, port, resume_offset);
-    err = irc_privmsg(sockfd, remote_nick, message);
+    err = irc_privmsg(sockfd, remote_nick, bdata(message));
 
     return err;
 }
