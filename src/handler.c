81c4f5d70cab423df87b6561fa7b78eeadff903c
Ricardo Martins <ricardo@scarybox.net>
2010-08-20 20:26:41 +0100

    Fixed off-by-one error.

--- src/handler.c
+++ src/handler.c
@@ -40,7 +40,7 @@ int handler(int sockfd, irc_request *irc_req)
             xdcc_list(irc_req->remote_nick, sockfd);
             break;
         case SEND:
-            if (irc_req->number > 0 && irc_req->number < nfiles)
+            if (irc_req->number > 0 && irc_req->number <= nfiles)
                 xdcc_send(&files[irc_req->number-1], irc_req->remote_nick, sockfd);
             break;
         default:
