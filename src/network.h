b0542df69eb4036c87d3211ade86b596d4259f68
Ricardo Martins <ricardo@scarybox.net>
2010-08-22 13:19:42 +0100

    Removed prototype for inexistent function.

--- src/network.h
+++ src/network.h
@@ -1,7 +1,6 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-void send_message(char *remote_nick, char *type, char *message, int sockfd);
 int create_socket(char *hostname, char *port);
 char *get_external_ip();
 
