2bb983c968f60b6a4a086c939b1585e8070cd092
Ricardo Martins <ricardo@scarybox.net>
2010-08-24 17:40:49 +0100

    Added include of <sys/socket.h> in globals.h.
    This fixes a compilation error in Linux, since MSG_NOSIGNAL is defined in
    <sys/socket.h> and was thus invisible from globals.h, conflicting then with the
    definition given by the inclusion of <sys/socket.h> in xdcc.h.
    
    I considered moving the ifdef to xdcc.c but would imply further copy-paste of
    the conditional definition if I wanted to use MSG_NOSIGNAL in other files, such
    as network.c or irc.c.

--- src/globals.h
+++ src/globals.h
@@ -3,6 +3,9 @@
 #ifndef globals_h
 #define globals_h
 
+#include <sys/socket.h>
+
+/* Compatibility for MSG_NOSIGNAL-deprived OSes, like OSX. */
 #ifndef MSG_NOSIGNAL
 #define MSG_NOSIGNAL 0
 #endif
